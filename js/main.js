333535c30d747dad4381df46cbc9fd5ee30b5040
Mershelle Rivera <mershellemae@gmail.com>
2025-07-31 08:58:26 -0700

    style display driver

--- js/main.js
+++ js/main.js
@@ -4,14 +4,23 @@ document.querySelector('button').addEventListener('click', getFetch)
 function getFetch(){
   const search = document.querySelector('input').value;
   const fullName = encodeURIComponent(getFullName(search));
+  const driverName = document.querySelector('.driver-name');
+  const driverImg = document.querySelector('img');
+  const displayDriver = document.querySelector('.display-driver');
+  const teamName = document.querySelector('.team-name');
   const url = `https://api.openf1.org/v1/drivers?full_name=${fullName}&session_key=latest`;
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data);
-        document.querySelector('h2').innerText = data[0].first_name + " " + data[0].last_name;
-        document.querySelector('img').src = data[0].headshot_url;
+        driverName.innerText = data[0].first_name + " " + data[0].last_name;
+        driverImg.src = data[0].headshot_url;
+        teamName.innerHTML = data[0].team_name;
+
+        if (displayDriver.classList.contains('hide')) {
+          displayDriver.classList.toggle('hide');
+        }
       })
       .catch(err => {
           console.log(`error ${err}`)
